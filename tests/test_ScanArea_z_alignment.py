d466c9f91ffe997649f86cb9292366c8aee9309f
thayer <David.W.Clemens-Sewall.Th@dartmouth.edu>
2021-03-12 19:14:28 -0500

    Found decent parameters for z_alignment

--- tests/test_ScanArea_z_alignment.py
+++ tests/test_ScanArea_z_alignment.py
@@ -24,39 +24,39 @@ Registration = namedtuple('Registration', ['project_name_0', 'project_name_1',
                                            'yaw_angle'], 
                           defaults=[None, None, None])
 
-project_names = [#'mosaic_rov_040120.RiSCAN',
-                 #'mosaic_rov_110120.RiSCAN',
+project_names = ['mosaic_rov_040120.RiSCAN',
+                 'mosaic_rov_110120.RiSCAN',
                  'mosaic_rov_190120.RiSCAN',
-                 'mosaic_rov_250120.RiSCAN'#,
-                 #'mosaic_rov_040220.RiSCAN',
-                 #'mosaic_rov_220220.RiSCAN.RiSCAN'
+                 'mosaic_rov_250120.RiSCAN',
+                 'mosaic_rov_040220.RiSCAN',
+                 'mosaic_rov_220220.RiSCAN.RiSCAN'
                  ]
 
 registration_list = [Registration('mosaic_rov_190120.RiSCAN', 
                                   'mosaic_rov_190120.RiSCAN'),
-                     # Registration('mosaic_rov_190120.RiSCAN',
-                     #              'mosaic_rov_110120.RiSCAN',
-                     #              ['r05', 'r28', 'r29', 'r31', 'r32', 'r33',
-                     #               'r34'],
-                     #              'LS'),
-                     # Registration('mosaic_rov_190120.RiSCAN',
-                     #              'mosaic_rov_040120.RiSCAN',
-                     #              ['r28', 'r29', 'r30', 'r31', 'r32', 'r33'],
-                     #              'LS'),
+                      Registration('mosaic_rov_190120.RiSCAN',
+                                   'mosaic_rov_110120.RiSCAN',
+                                   ['r05', 'r28', 'r29', 'r31', 'r32', 'r33',
+                                    'r34'],
+                                   'LS'),
+                      Registration('mosaic_rov_190120.RiSCAN',
+                                   'mosaic_rov_040120.RiSCAN',
+                                   ['r28', 'r29', 'r30', 'r31', 'r32', 'r33'],
+                                   'LS'),
                       Registration('mosaic_rov_190120.RiSCAN',
                                    'mosaic_rov_250120.RiSCAN',
                                    ['r28', 'r29', 'r30', 'r32', 'r34', 'r35',
                                     'r22'],
-                                   'LS')#,
-                      # Registration('mosaic_rov_250120.RiSCAN',
-                      #              'mosaic_rov_040220.RiSCAN',
-                      #              ['r28', 'r29', 'r30', 'r31', 'r32', 'r34', 
-                      #               'r35', 'r36'],
-                      #              'LS'),
-                      # Registration('mosaic_rov_040220.RiSCAN',
-                      #              'mosaic_rov_220220.RiSCAN.RiSCAN',
-                      #              ['r28', 'r31', 'r32', 'r34'],
-                      #              'Yaw')
+                                   'LS'),
+                       Registration('mosaic_rov_250120.RiSCAN',
+                                    'mosaic_rov_040220.RiSCAN',
+                                    ['r28', 'r29', 'r30', 'r31', 'r32', 'r34', 
+                                     'r35', 'r36'],
+                                    'LS'),
+                       Registration('mosaic_rov_040220.RiSCAN',
+                                    'mosaic_rov_220220.RiSCAN.RiSCAN',
+                                    ['r28', 'r31', 'r32', 'r34'],
+                                    'Yaw')
                       ]
 
 # %% Init
@@ -72,7 +72,8 @@ scan_area.register_all()
 
 # %% align two scans in sub_list
 
-scan_area.z_align_all(frac_exceed_diff_cutoff=0.25,
+scan_area.z_align_all(frac_exceed_diff_cutoff=0.25, w0=1, w1=1, min_pt_dens=25,
+                      max_diff=0.2,
                       bin_reduc_op='mean', diff_mode='mode')
 
 # %% Let's look at what some of the z offsets look like
